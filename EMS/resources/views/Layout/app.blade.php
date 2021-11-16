a4ace6ff3555d3312a4d8fba631228c73f5e0c38
Reajur Rahaman <65109086+Rumman98@users.noreply.github.com>
2021-11-16 14:46:12 +0600

    Footer Update
    Footer updated with css

--- EMS/resources/views/Layout/app.blade.php
+++ EMS/resources/views/Layout/app.blade.php
@@ -15,6 +15,7 @@
 <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
 <link rel="stylesheet" href="{{asset('css/font-icon.css')}}">
 <link rel="stylesheet" href="{{asset('css/animate.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
 {{-- <link rel="stylesheet" href="../../../maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> --}}
 </head>
 
