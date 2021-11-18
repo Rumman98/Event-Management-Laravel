a105cc2e76aa5dcb70608605bc8b437720f50956
Reajur Rahaman <65109086+Rumman98@users.noreply.github.com>
2021-11-18 11:21:13 +0600

    nav logo changed

--- EMS/resources/views/Layout/menu.blade.php
+++ EMS/resources/views/Layout/menu.blade.php
@@ -1,11 +1,12 @@
 <header id="header">
-<div class="header-content clearfix"> <span class="logo"><a href="/">BEST<b>Events</b></a></span>
+<div class="header-content clearfix"> <span class="logo"><a href="/">Unity<b>Events</b></a></span>
 <nav class="navigation" role="navigation">
     <ul class="primary-nav">
       <li><a href="{{'/'}}">Home</a></li>
       <li><a href="{{'/running-events'}}">Running Events</a></li>
       <li><a href="#gallery">Gallery</a></li>
       <li><a href="#package">Login/Signup</a></li>
+      <li><a href="#package">Login/Signup As a Host</a></li>
       <li><a href="#contact">Contact Us</a></li>
     </ul>
   </nav>
