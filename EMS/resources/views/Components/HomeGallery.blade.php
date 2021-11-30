53b8385f4ef81940b87eae4b97dd2f1e5b83ef3c
Reajur Rahaman <65109086+Rumman98@users.noreply.github.com>
2021-11-30 21:56:33 +0600

    small changes

--- EMS/resources/views/Components/HomeGallery.blade.php
+++ EMS/resources/views/Components/HomeGallery.blade.php
@@ -11,7 +11,6 @@
                <p>image description</p>
             </div>
           </div>
-          <!-- overlay -->
           </a>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/portfolio/02.jpg" class="work-box"> <img src="images/portfolio/02.jpg" alt="">
@@ -20,7 +19,6 @@
               <p><span class="icon icon-magnifying-glass"></span></p>
             </div>
           </div>
-          <!-- overlay -->
           </a> </div>
         <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/portfolio/03.jpg" class="work-box"> <img src="images/portfolio/03.jpg" alt="">
           <div class="overlay">
@@ -28,7 +26,6 @@
               <p><span class="icon icon-magnifying-glass"></span></p>
             </div>
           </div>
-          <!-- overlay -->
           </a> </div>
         <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/portfolio/04.jpg" class="work-box"> <img src="images/portfolio/04.jpg" alt="">
           <div class="overlay">
@@ -36,7 +33,6 @@
               <p><span class="icon icon-magnifying-glass"></span></p>
             </div>
           </div>
-          <!-- overlay -->
           </a> </div>
         <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/portfolio/05.jpg" class="work-box"> <img src="images/portfolio/05.jpg" alt="">
           <div class="overlay">
@@ -44,7 +40,6 @@
               <p><span class="icon icon-magnifying-glass"></span></p>
             </div>
           </div>
-          <!-- overlay -->
           </a> </div>
         <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/portfolio/06.jpg" class="work-box"> <img src="images/portfolio/06.jpg" alt="">
           <div class="overlay">
@@ -52,7 +47,6 @@
               <p><span class="icon icon-magnifying-glass"></span></p>
             </div>
           </div>
-          <!-- overlay -->
           </a> </div>
         <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/portfolio/07.jpg" class="work-box"> <img src="images/portfolio/07.jpg" alt="">
           <div class="overlay">
@@ -60,7 +54,6 @@
               <p><span class="icon icon-magnifying-glass"></span></p>
             </div>
           </div>
-          <!-- overlay -->
           </a> </div>
         <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/portfolio/08.jpg" class="work-box"> <img src="images/portfolio/08.jpg" alt="">
           <div class="overlay">
@@ -68,7 +61,6 @@
                <p><span class="icon icon-magnifying-glass"></span></p>
             </div>
           </div>
-          <!-- overlay -->
           </a>
         </div>
       </div>
