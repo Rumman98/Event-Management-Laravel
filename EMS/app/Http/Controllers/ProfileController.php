66a13506ec9fa5ed7eb98f7d07cf1ff6b8cf5e70
Abdul Jabbar Gazi <abdul35-2062@diu.edu.bd>
2021-12-08 19:31:00 +0600

    Update ProfileController.php

--- EMS/app/Http/Controllers/ProfileController.php
+++ EMS/app/Http/Controllers/ProfileController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\DB;
 use App\UserHostModel;
 use App\EventRegistrationModel;
 
